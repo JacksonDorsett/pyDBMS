18c7b5ce0d4c69368dcb2b23f884254d13482910
Jackson <jackson.dorsett@economicmodeling.com>
2022-01-09 13:12:38 -0800

    updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pyDBMS",
-    version="0.1.1",
+    version="0.1.2",
     description="Easy to use, light weight ORM",
     long_description=README,
     long_description_content_type="text/markdown",
